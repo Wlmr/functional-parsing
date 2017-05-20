127acade8f4ae5934a052b7d1b04bcd92c2663a2
wlmr <wilmernilsson@gmail.com>
2017-05-20 21:34:21 +0200

    ssssssssss

--- src/Program.hs
+++ src/Program.hs
@@ -2,10 +2,21 @@ module Program(T, parse, fromString, toString, exec) where
 import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
+import Data.List
 import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
+newtype T = Program [Statement.T] deriving Show
+
+statements :: Parser T
+statements = iter Statement.parse >-> buildStatements
+buildStatements :: [Statement.T] -> T
+buildStatements = Program
+
 instance Parse T where
-  parse = error "Program.parse not implemented"
-  toString = error "Program.toString not implemented"
-             
-exec = error "Program.exec not implemented"
+    parse = iter Statement.parse >-> Program
+    toString (Program stmts) = intercalate "\n" $ map toString stmts
+
+exec :: T -> [Integer] -> [Integer]
+exec (Program stmts) = Statement.exec stmts Dictionary.empty
+
+-- toString' :: T -> String
+-- toString' (Program stmts) = concatMap Statement.toString stmts
--- src/Statement.hs
+++ src/Statement.hs
@@ -6,38 +6,39 @@ import qualified Expr
 type T = Statement
 data Statement =
     Skip |
-    Comment |
     Read' String |
     Write Expr.T |
+    Comment String |
     While Expr.T Statement |
     Assignment String Expr.T |
     If Expr.T Statement Statement |
     Block [Statement]
     deriving Show
 
-comment = (accept "--" # require "\n") >-> buildComment
-buildComment _ = Comment
+comment = accept "--" -# iter (word "\\n") #- require "\n" >-> buildComment
+buildComment s = Comment s
 
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
 
-skip = (accept "skip" # require ";") >-> buildSkip
+skip = (accept "skip" -# require ";") >-> buildSkip
 buildSkip _ = Skip
 
-read'= accept "read " -# word #- require ";" >-> buildRead'
-buildRead' = Read'
-
-block = accept "begin" -# iter statement #- require "end" >-> buildBlock
+block = accept "begin" -# iter parse #- require "end" >-> buildBlock
 buildBlock = Block
 
-if' = accept "if" -# Expr.parse #- require "then" # (statement #- require "else" # statement) >-> buildIf'
-buildIf' (condition,(th,el)) = If condition th el
+if' = accept "if" -# Expr.parse #- require "then" # parse #- require "else" # parse >-> buildIf'
+buildIf' ((condition,th),el) = If condition el th
 
-while = accept "while" -# Expr.parse #- require "do" # statement >-> buildWhile
-buildWhile (ex,st) = While ex st
+while = accept "while" -# Expr.parse #- require "do" # parse >-> buildWhile
+buildWhile (cond, stmt) = While cond stmt
+
+read' = accept "read" -# word #- require ";" >-> buildRead
+buildRead = Read'
 
 write = accept "write" -# Expr.parse #- require ";" >-> buildWrite
 buildWrite = Write
+
 
 statement :: Parser Statement
 statement = assignment ! skip ! read' ! block ! if' ! while ! write
@@ -49,39 +50,42 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     then exec (thenStmts: stmts) dict input
     else exec (elseStmts: stmts) dict input
 exec (Assignment var expr : stmts) dict input =
-  exec stmts (Dictionary.insert (var, Expr.value expr dict) dict) input
+    exec stmts (Dictionary.insert (var, Expr.value expr dict) dict) input
 exec (Skip : stmts) dict input = exec stmts dict input
 exec (Block block : stmts) dict input = exec (block++stmts) dict input
 exec loop@(While cond body : after) dict input =
     if Expr.value cond dict > 0
-        then exec (body : loop) dict input
-        else exec after dict input
+    then exec (body : loop) dict input
+    else exec after dict input
 exec (Read' var : stmts) dict (input:rest) =
     exec stmts (Dictionary.insert (var, input) dict) rest
 exec (Write expr : stmts) dict input =
     Expr.value expr dict : exec stmts dict input
+exec (Comment str : stmts) dict input =
+    exec stmts dict input
 
 instance Parse Statement where
   parse = statement
   toString = toString' 0
 
-tabsize = 4
+tabsize = 2
 
 toString' :: Int -> Statement -> String
 toString' indent (Assignment var expr) =
-    replicate indent ' ' ++ var ++ ":=" ++ toString expr ++ ";\n"
-toString' indent Skip = replicate indent ' ' ++ "skip;\n"
+    replicate indent ' ' ++ var ++ ":=" ++ toString expr ++ ";"
+toString' indent Skip = replicate indent ' ' ++ "skip;"
 toString' indent (Block block) =
-    replicate indent ' ' ++ "begin\n"
-    ++ concat (map (toString' (indent+tabsize)) block)
-    ++ replicate indent ' ' ++ "end\n"
+    replicate indent ' ' ++ "begin"
+    ++ concatMap (toString' (indent+tabsize)) block
+    ++ replicate indent ' ' ++ "end"
 toString' indent (If cond th el) =
-    replicate indent ' ' ++ "if " ++ Expr.toString cond ++ " then\n"
+    replicate indent ' ' ++ "if" ++ Expr.toString cond ++ "then\n"
     ++ toString' (indent+tabsize) th ++ replicate indent ' '
     ++ "else\n" ++ toString' (indent+tabsize) el
 toString' indent (While cond body) =
-    replicate indent ' ' ++ "while " ++ Expr.toString cond ++ " do\n"
+    "while " ++ Expr.toString cond ++ "do\n"
     ++ toString' (indent+tabsize) body
-toString' indent (Read' var) = replicate indent ' ' ++ "read " ++ var ++ ";\n"
+toString' indent (Read' var) = replicate indent ' ' ++ "read" ++ var ++ ";\n"
 toString' indent (Write expr) =
-    replicate indent ' ' ++ "write " ++ Expr.toString expr ++ ";\n"
+    replicate indent ' ' ++ "write" ++ Expr.toString expr ++ ";"
+toString' indent (Comment s) = replicate indent ' ' ++ "--" ++ s
