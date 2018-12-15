5db7d9942679280e7db4659de72a3ce7e3274443
Wilmer Nilsson <wlmr@pm.me>
2018-12-15 18:31:11 +0100

    revisited

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import Program
+import System.Environment
+import System.IO
+import System.Directory
+import Control.Monad
 
 main :: IO ()
-main = someFunc
+main = inputChecker [""] >>= (\(program:variables) -> print $ "Results: " ++ (show . Program.exec (fromString program)) (read (head variables)))
+
+inputChecker :: [String] -> IO [String]
+inputChecker (prog:vars) = doesFileExist prog >>= (\exists -> if exists then readFile prog >>= (\content -> return (content : vars)) else return (prog:vars))
+inputChecker whatever    = sequence [putStr "\nEnter filename or program\n" >> getLine, putStr "Enter variables [v1,v2,...,vN]\n" >> getLine] >>= inputChecker 
+                            
+
+
+
