803b02dd8d19803ff29025ce25713b8ce687bb67
Wilmer Nilsson <wlmr@pm.me>
2018-12-15 20:34:04 +0100

    Done

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,15 @@
 module Main where
 
 import Program
-import System.Environment
 import System.IO
 import System.Directory
-import Control.Monad
 
 main :: IO ()
-main = inputChecker [""] >>= (\(program:variables) -> print $ "Results: " ++ (show . Program.exec (fromString program)) (read (head variables)))
+main = inputChecker [] >>= (\(program:variables) -> (putStrLn . (++) "\nResults: " . show . Program.exec (fromString program) . read . head) variables)
 
 inputChecker :: [String] -> IO [String]
-inputChecker (prog:vars) = doesFileExist prog >>= (\exists -> if exists then readFile prog >>= (\content -> return (content : vars)) else return (prog:vars))
-inputChecker whatever    = sequence [putStr "\nEnter filename or program\n" >> getLine, putStr "Enter variables [v1,v2,...,vN]\n" >> getLine] >>= inputChecker 
+inputChecker (prog:vars) = doesFileExist prog >>= (\exists -> if exists then readFile prog >>= (\content -> return (content:vars)) else return (prog:vars))
+inputChecker  something  = sequence [putStrLn "Enter filepath or program: " >> getLine, putStrLn "\nEnter variables, e.g. [v1,...,vN]" >> getLine] >>= inputChecker 
                             
 
 
